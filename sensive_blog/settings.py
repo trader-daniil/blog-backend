1422bfb06c90753a201b34a5db843aac2e3ea822
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-03-26 17:11:59 +0300

    rm media dir

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -92,17 +92,25 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = os.getenv('STATIC_URL', '/static/')
+#STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = [
+    'test_static',
+]
 
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
+
 
 STATIC_ROOT = os.getenv("STATIC_ROOT")
 
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+
 
 INTERNAL_IPS = [
     '127.0.0.1',
